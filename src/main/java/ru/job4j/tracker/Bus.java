a82607c236e5f756305f3e97f23366e7a6e65176
Anatoly Busygin <anatolyb94@gmail.com>
2023-02-15 22:20:36 +0500

    Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        System.out.println("drive");
+    }
+
+    @Override
+    public void passengers(int count) {
+        System.out.println("passengers: " + count);
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
