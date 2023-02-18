6c74233e718360a8e2d172745e36f2d27996d4b5
Anatoly Busygin <anatolyb94@gmail.com>
2023-02-18 20:44:37 +0500

    Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -25,4 +25,31 @@ public class StartUITest {
         Item expected = new Item("test");
         assertThat(created.getName()).isNotEqualTo(expected.getName());
     }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        String[] answers = {"test"};
+        Input input = new StubInput(answers);
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        int id = created.getId();
+        String[] deleteAnswers = {String.valueOf(id)};
+        StartUI.deleteItem(new StubInput(deleteAnswers), tracker);
+        assertThat(tracker.findById(id)).isEqualTo(null);
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
 }
